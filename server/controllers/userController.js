c50dbbe587e5287493a41164c313b039fd3787bd
rishab <rishab.mehta12480@gmail.com>
2024-06-22 16:33:46 +0530

    minor fix

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -85,5 +85,18 @@ const findUser = async(req,res) =>
     }
 } 
 
+const getUsers = async(req,res) =>
+{
+    try{
+        const users=await userModel.find();
+        res.status(200).json(users);
+    }
+    catch(error)
+    {
+        console.error("An unknown error occured",error);
+        res.status(500).json(error);
+    }
+}
 
-module.exports={registerUser,loginUser,findUser};
+
+module.exports={registerUser,loginUser,findUser,getUsers};
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,7 +1,7 @@
 const express=require("express");
 const router=express.Router();
 
-const {registerUser,loginUser,findUser}=require("../controllers/userController");
+const {registerUser,loginUser,findUser,getUsers}=require("../controllers/userController");
 
 router.post("/register",registerUser);
 
@@ -9,5 +9,7 @@ router.post("/login",loginUser);
 
 router.get("/find/:userId",findUser);
 // These are path params
+
+router.get("/",getUsers);
 
 module.exports=router;
