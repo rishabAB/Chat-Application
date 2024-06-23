092d8aecc01777c00676960cd7de6397a8f3e4c7
rishab <rishab.mehta12480@gmail.com>
2024-06-23 17:00:53 +0530

    context api , login and register ui

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,11 +4,13 @@ import App from './App.jsx'
 import './index.css'
 
 import {BrowserRouter} from 'react-router-dom';
-
+import {AuthContextProvider} from './context/authContext';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
+    <AuthContextProvider>
     <App />
+    </AuthContextProvider>
     </BrowserRouter>
    
   </React.StrictMode>,
--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -1,5 +1,34 @@
+import {Alert,Button,Form,Row,Col,Stack} from "react-bootstrap";
+
 const Login = () => {
-    return ( <>Login</>);
+    return (
+        <>
+            <Form>
+                <Row style={{
+                    height:"100vh",
+                justifyContent:"center",
+                paddingTop:"10%"
+                }}>
+
+                    <Col xs={6}>
+                        <Stack gap={3}>
+                            <h2>Login</h2>
+                           
+                            <Form.Control type="email" placeholder="Email"/>
+                            <Form.Control type="password" placeholder="Password"/>
+                            <Button varient="primary" type="submit">
+                                Login
+                            </Button>
+                            <Alert varient="danger">
+                                <p>An error occurred</p>
+                            </Alert>
+                        </Stack>
+                    </Col>
+                </Row>
+            </Form>
+
+        </>
+    );
 }
- 
+
 export default Login;
