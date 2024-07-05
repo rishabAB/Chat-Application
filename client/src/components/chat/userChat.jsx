1980b7685e47f02a64ffeff03e1bc2583d368af1
rishab <rishab.mehta12480@gmail.com>
2024-07-05 17:38:09 +0530

    minor chat fixes

--- client/src/components/chat/userChat.jsx
+++ client/src/components/chat/userChat.jsx
@@ -1,11 +1,28 @@
 import {useFetchRecipientUser} from "../../hooks/useFetchRecipient";
+import {Stack} from "react-bootstrap";
 
-const UserChat = ({chat,user}) => {
-    const {recipientUser} = useFetchRecipientUser(chat,user);
+const UserChat = ({chat, user}) => {
+    const {recipientUser} = useFetchRecipientUser(chat, user);
 
-    console.log("dsjd",recipientUser);
+    console.log("dsjd", recipientUser);
 
-    return ( <>UserChat tests</> );
+    return (
+        <Stack direction="horizontal"
+            gap={3}
+            className="user-card align-items-center p-2 justify-content-between">
+            <div className="d-flex">
+                <div className="me-2"></div>
+                <div className="text-content">
+                    <div className="name">
+                        {
+                        recipientUser ?. name
+                    }</div>
+                    <div className="text">text Message</div>
+
+                </div>
+            </div>
+        </Stack>
+    );
 }
- 
+
 export default UserChat;
--- client/src/pages/chat.jsx
+++ client/src/pages/chat.jsx
@@ -1,40 +1,40 @@
 import {useContext} from "react";
-import {Container,Stack} from "react-bootstrap";
+import {Container, Stack} from "react-bootstrap";
 import {ChatContext} from "../context/chatContext";
 import {AuthContext} from "../context/authContext";
 import UserChat from "../components/chat/userChat";
 const Chat = () => {
-    const { user
-         
-        } = useContext(AuthContext);
+    const {user} = useContext(AuthContext);
 
-        const { userChats,
-            isUserChatLoading, 
-            isUserChatError} = useContext(ChatContext);
+    const {userChats, isUserChatLoading, isUserChatError} = useContext(ChatContext);
 
-            console.log("userChats",userChats);
+    console.log("userChats", userChats);
 
-   
-    return (  <Container>{userChats?.length<1 ? null : (
-    <Stack direction="horizontal" gap ={4} className="align-items-start">
-        <Stack className="messages-box flex-grow-0 pe-3">
-            {isUserChatLoading && <p>Loading Chats...</p>}
-           {
-            userChats?.((chat,index) =>
-            {
-                return (<div key={index}>
-                    <UserChat chat ={chat} user={user}/>
-                </div>)
-            })
 
-           }
-        </Stack>
-        <p>Chatbox</p>
+    return (
+        <Container>{
+            userChats ?. length < 1 ? null : (
+                <Stack direction="horizontal" gap ={4} className="align-items-start">
+                    <Stack className="messages-box flex-grow-0 pe-3">
+                        {
+                        isUserChatLoading && <p>Loading Chats...</p>
+                    }
+                        {
+                        userChats ?. map((chat, index) => {
+                            return (
+                                <div key={index}>
+                                    <UserChat chat ={chat}
+                                        user={user}/>
+                                </div>
+                            )
+                        })
+                    } </Stack>
+                    <p>Chatbox</p>
 
-    </Stack>
-    )
-    }</Container>
+                </Stack>
+            )
+        }</Container>
     );
 }
- 
+
 export default Chat;
