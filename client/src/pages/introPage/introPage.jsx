698c220cb7f7bbb78d2470791d06c4299a5505bf
Rishab <rishab.mehta@antiersolutions.com>
2024-10-06 20:26:24 +0530

    deploy:test

--- client/src/pages/introPage/introPage.jsx
+++ client/src/pages/introPage/introPage.jsx
@@ -17,8 +17,9 @@ const IntroPage = ({ isRegister }) => {
   return (
     <div className="register_login">
       <div className="register_login_img_div">
-        <img src="./3394897.jpg" alt="" />
+         <img src="./3394897.jpg" alt="" />
         {/* <img src="../../assets/3394897.jpg" alt="" /> */}
+        {/* Test */}
       </div>
 
       <div>{isRegisterProp ? <Register /> : <Login />}</div>
